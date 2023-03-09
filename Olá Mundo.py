b157fee2114cde66f8531a5c2b934f27dcfbe200
Davi Solo <98850348+DaviSolo7@users.noreply.github.com>
2023-03-09 20:00:22 -0300

    Add files via upload

---
+++ Olá Mundo.py
@@ -0,0 +1,9 @@
+### Programa que imprime: "Olá Mundo"
+### Autor: Davi Solo 
+### Data: 01/03/2023
+### Versão: 2.0
+
+# Início
+
+print('\t\t\t---Primeiro Código---\n\n')
+print('olá mundo'.title())
