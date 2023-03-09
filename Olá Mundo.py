060cd05b14d1e416d8bd83e2b3199057b64194a7
Davi Solo <98850348+DaviSolo7@users.noreply.github.com>
2023-03-09 20:10:07 -0300

    Delete Olá Mundo.py

--- Olá Mundo.py
+++
@@ -1,9 +0,0 @@
-### Programa que imprime: "Olá Mundo"
-### Autor: Davi Solo 
-### Data: 01/03/2023
-### Versão: 2.0
-
-# Início
-
-print('\t\t\t---Primeiro Código---\n\n')
-print('olá mundo'.title())
